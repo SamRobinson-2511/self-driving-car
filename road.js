3f219352f80142c58b69cb5a9f0c96bd1facf910
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-07-13 23:49:49 -0500

    center car on lane
    using linear interpolation to keep things centered

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-  constructor(x, width, laneCount=3){
+  constructor(x, width, laneCount=4){
     this.x=x;
     this.width=width;
     this.laneCount=laneCount;
@@ -12,19 +12,31 @@ class Road{
     this.bottom=infinity;
   }
 
+  getLaneCenter(laneIndex) {
+    const laneWidth=this.width/this.laneCount;
+    return this.left+laneWidth/2+laneIndex*laneWidth;
+  }
+
   draw(ctx){
     ctx.lineWidth=5;
     ctx.strokeStyle="white";
 
-    ctx.beginPath();
-    ctx.moveTo(this.left, this.top);
-    ctx.lineTo(this.left, this.bottom);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(this.right, this.top);
-    ctx.lineTo(this.right, this.bottom);
-    ctx.stroke();
-
+    for(let i=0;i<=this.laneCount;i++){
+      const x=lerp(
+        this.left, 
+        this.right,
+        i/this.laneCount
+      );
+      if(i>0 && i<this.laneCount){
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
 }
+
